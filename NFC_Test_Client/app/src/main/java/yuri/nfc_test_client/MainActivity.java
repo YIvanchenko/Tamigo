50e5ec457f8683ad8ca6cb22fd68d99d8b47228a
Yuri Ivanchenko <yuri.ivanchenko.ua@gmail.com>
2015-08-31 09:31:09 +0200

    The simplest Android client that reads NFC tag and asks the server about user details.

---
+++ NFC_Test_Client/app/src/main/java/yuri/nfc_test_client/MainActivity.java
@@ -0,0 +1,236 @@
+package yuri.nfc_test_client;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpResponse;
+
+public class MainActivity extends AppCompatActivity {
+    private NfcAdapter mNfcAdapter;
+    private String nfcId;
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled())
+            Toast.makeText(this, "NFC is disabled.", Toast.LENGTH_LONG).show();
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            new NfcReadTask().execute(tag);
+        }
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
+    private class HttpGetTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            TextView mTextView = (TextView) findViewById(R.id.mTextView);
+
+            if (result != null &&result.replace("\"", "").length() != 0) {
+                mTextView.setText(String.format("Your Rejsekort is assigned to an account %s", result));
+            } else {
+                mTextView.setText("Your Rejsekort is not registered in the database");
+            }
+        }
+    }
+
+    private class NfcReadTask extends AsyncTask<Tag, Void, String> {
+
+        private String convertByteArrayToHexString (byte[] b) {
+            if (b != null) {
+                StringBuilder s = new StringBuilder(2 * b.length);
+                for (int i = 0; i < b.length; ++i) {
+                    final String t = Integer.toHexString(b[i]);
+                    final int l = t.length();
+                    if (l > 2) {
+                        s.append(t.substring(l - 2));
+                    } else {
+                        if (l == 1) {
+                            s.append("0");
+                        }
+                        s.append(t);
+                    }
+                }
+                return s.toString();
+            } else {
+                return "";
+            }
+        }
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+            byte[] uid = tag.getId();
+
+            return convertByteArrayToHexString(uid);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                String serverUrl = getResources().getString(R.string.serverUrl);
+                new HttpGetTask().execute(String.format(serverUrl, result));
+            } else {
+                TextView mTextView = (TextView) findViewById(R.id.mTextView);
+                mTextView.setText("Cannot read your Rejsekort");
+            }
+        }
+    }
+}
