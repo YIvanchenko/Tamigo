755d9110493bcf244ab78dbf3af24d17ebd11d49
Yuri Ivanchenko <yuri.ivanchenko.ua@gmail.com>
2015-08-31 08:48:06 +0200

    The simplest server that returns user details by a given user.

---
+++ NFC_Test/UserInfoServer/Controllers/UserDetailsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using UserInfoServer.Models;
+
+namespace UserInfoServer.Controllers
+{
+    public class UserDetailsController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+        private UserNfcContext _userNfcContext = new UserNfcContext();
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: api/UserDetails/5
+        public string Get(string nfc)
+        {
+            var userNfc = _userNfcContext.UserNfcs.FirstOrDefault(un => un.Nfc == nfc);
+            if (userNfc != null) 
+            {
+                var user = UserManager.FindById(userNfc.UserId);
+                return user.Email;
+            }
+
+            return string.Empty;
+        }
+    }
+}
